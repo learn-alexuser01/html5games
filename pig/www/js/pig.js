d4c21585f09e33a12c2c6834683bb36f695df07d
Scott Davis <scott@thirstyhead.com>
2013-09-30 22:32:00 -0600

    added pig.checkForWinner()

--- pig/www/js/pig.js
+++ pig/www/js/pig.js
@@ -1,7 +1,8 @@
 function Pig(name){
   this.p1 = new Player("p1"),
   this.p2 = new Player("p2"),
-  this.currentPlayer = this.p1
+  this.currentPlayer = this.p1,
+  this.winningScore = 100
 }
 
 Pig.prototype.swapPlayers = function(){
@@ -43,8 +44,6 @@ Pig.prototype.swapPlayers = function(){
   var rollButton = document.getElementById(this.currentPlayer.name + "_rollButton") 
   rollButton.removeAttribute("disabled") 
 
-
-
   return this.currentPlayer
 }
 
@@ -66,5 +65,48 @@ Pig.prototype.roll = function(){
     holdButton.removeAttribute("disabled")
   }
   
+  return result
+}
+
+
+Pig.prototype.hold = function(){
+  this.currentPlayer.hold()
+
+  // update webpage
+  var bucketDiv = document.getElementById(this.currentPlayer.name + "_bucket")
+  bucketDiv.innerHTML = this.currentPlayer.bucket
+  var scoreDiv = document.getElementById(this.currentPlayer.name + "_score")
+  scoreDiv.innerHTML = this.currentPlayer.score
+
+  if(!this.checkForWinner()){
+    this.swapPlayers()  
+  }
+}
+
+Pig.prototype.checkForWinner = function(){
+  var result = false
+  if(this.currentPlayer.score >= this.winningScore){
+    // add border
+    var currentPlayer = document.getElementById(this.currentPlayer.name) 
+    currentPlayer.className = currentPlayer.className + " winner"
+
+    // add "WINNER"
+    var h2 = document.querySelector(".currentPlayer h2")
+    h2.insertAdjacentHTML("beforeEnd", " -- WINNER!!!")
+
+    // disable all buttons
+    var buttons = ["p1_rollButton", 
+                   "p1_holdButton", 
+                   "p2_rollButton", 
+                   "p2_holdButton"]
+
+    for(var i=0; i<buttons.length; i++){
+      var button = document.getElementById(buttons[i])
+      button.setAttribute("disabled")
+    }               
+
+    result = true  
+  }
+
   return result
 }
